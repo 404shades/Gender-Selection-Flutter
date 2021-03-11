e1bd24cd061d2e2764a99aa07fe33e6d5cb7dc93
Vytautas Pranskunas <vytautas.pranskunas@ef.com>
2021-03-11 11:29:05 +0100

    Added third gender

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:gender_selection/gender_selection.dart';
 import 'package:example/utils/gradients.dart';
+import 'package:gender_selection/gender_selection.dart'
+
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,14 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gender Selection Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Colors.red,
-      ),
-      home: HomeScreen()
-    );
+        title: 'Gender Selection Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.red,
+        ),
+        home: HomeScreen());
   }
 }
 
@@ -23,67 +24,63 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Gender Selection Demo"),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              GenderSelection(
-                femaleImage: NetworkImage("https://cdn1.iconfinder.com/data/icons/website-internet/48/website_-_female_user-512.png"),
-                maleImage: NetworkImage("https://icon-library.net/images/avatar-icon/avatar-icon-4.jpg"),
-                selectedGenderIconBackgroundColor: Colors.amber,
-                selectedGenderTextStyle: TextStyle(color: Colors.amber,fontSize: 19,fontWeight: FontWeight.bold),
-              ),
-              GenderSelection(
-                maleText: "",
-                femaleText: "",
-                linearGradient: pinkRedGradient,
-                selectedGenderIconBackgroundColor: Colors.indigo,
-                checkIconAlignment: Alignment.centerRight,
-                selectedGenderCheckIcon: null,
-                onChanged: (Gender gender){
-                  print(gender);
-                },
-                equallyAligned: true,
-                animationDuration: Duration(milliseconds: 400),
-                isCircular: true,
-                isSelectedGenderIconCircular: true,
-                opacityOfGradient: 0.6,
-                padding: const EdgeInsets.all(3),
-                size: 120,
-                
-              ),
-              GenderSelection(
-                maleImage: AssetImage("assets/images/male.png"),
-                femaleImage: AssetImage("assets/images/female.png"),
-                selectedGenderIconBackgroundColor: Colors.green,
-                selectedGenderIconColor: Colors.white,
-                linearGradient: greenSexyGradient,
-                onChanged: (Gender gender){
-                  print(gender);
-                },
-
-              ),
-              GenderSelection(
-                isCircular: false,
-                selectedGenderIconBackgroundColor: Colors.indigo,
-                linearGradient: skyBlueGradient,
-                onChanged: (Gender gender){
-                  print(gender);
-                },
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: Text("Gender Selection Demo"),
         ),
-      )
-    );
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                GenderSelection(
+                  femaleImage: NetworkImage(
+                      "https://cdn1.iconfinder.com/data/icons/website-internet/48/website_-_female_user-512.png"),
+                  maleImage: NetworkImage("https://icon-library.net/images/avatar-icon/avatar-icon-4.jpg"),
+                  selectedGenderIconBackgroundColor: Colors.amber,
+                  selectedGenderTextStyle: TextStyle(color: Colors.amber, fontSize: 19, fontWeight: FontWeight.bold),
+                ),
+                GenderSelection(
+                  maleText: "",
+                  femaleText: "",
+                  linearGradient: pinkRedGradient,
+                  selectedGenderIconBackgroundColor: Colors.indigo,
+                  checkIconAlignment: Alignment.centerRight,
+                  selectedGenderCheckIcon: null,
+                  onChanged: (Gender gender) {
+                    print(gender);
+                  },
+                  equallyAligned: true,
+                  animationDuration: Duration(milliseconds: 400),
+                  isCircular: true,
+                  isSelectedGenderIconCircular: true,
+                  opacityOfGradient: 0.6,
+                  padding: const EdgeInsets.all(3),
+                  size: 120,
+                ),
+                GenderSelection(
+                  maleImage: AssetImage("assets/images/male.png"),
+                  femaleImage: AssetImage("assets/images/female.png"),
+                  thirdGenderImage: AssetImage("assets/images/third_gender.png"),
+                  enableThirdGender: true,
+                  selectedGenderIconBackgroundColor: Colors.green,
+                  selectedGenderIconColor: Colors.white,
+                  linearGradient: greenSexyGradient,
+                  onChanged: (Gender gender) {
+                    print(gender);
+                  },
+                ),
+                GenderSelection(
+                  isCircular: false,
+                  selectedGenderIconBackgroundColor: Colors.indigo,
+                  linearGradient: skyBlueGradient,
+                  onChanged: (Gender gender) {
+                    print(gender);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
-
-
-
-
--- lib/gender_selection.dart
+++ lib/gender_selection.dart
@@ -3,7 +3,7 @@ library gender_selection;
 import 'package:flutter/material.dart';
 import 'package:gender_selection/utils/gradients.dart';
 
-enum Gender { Male, Female }
+enum Gender { Male, Female, ThirdGender }
 
 // ignore: must_be_immutable
 class GenderSelection extends StatefulWidget {
@@ -11,10 +11,12 @@ class GenderSelection extends StatefulWidget {
   final LinearGradient linearGradient;
   final ImageProvider maleImage;
   final ImageProvider femaleImage;
+  final ImageProvider thirdGenderImage;
   final ValueChanged<Gender> onChanged;
   final String maleText;
   final bool equallyAligned;
   final String femaleText;
+  final String thirdGenderText;
   final IconData selectedGenderCheckIcon;
   final double size;
   final bool isCircular;
@@ -28,41 +30,43 @@ class GenderSelection extends StatefulWidget {
   final TextStyle selectedGenderTextStyle;
   final TextStyle unSelectedGenderTextStyle;
   final Alignment checkIconAlignment;
+  final bool enableThirdGender;
 
-  GenderSelection(
-      {Key key,
-      this.selectedGenderTextStyle = const TextStyle(
-        fontSize: 19,
-        color: Colors.redAccent,
-        fontWeight: FontWeight.w600,
-      ),
-      this.unSelectedGenderTextStyle = const TextStyle(
-        fontSize: 19,
-        color: Colors.black,
-        fontWeight: FontWeight.w600,
-      ),
-      this.selectedGenderIconBackgroundColor = Colors.red,
-      this.selectedGenderIconColor = Colors.white,
-      this.selectedGenderIconSize = 20.0,
-      this.isSelectedGenderIconCircular = true,
-      this.animationDuration = const Duration(milliseconds: 500),
-      this.opacityOfGradient = 0.6,
-      this.padding = const EdgeInsets.all(10),
-      this.isCircular = true,
-      this.size = 120.0,
-      this.selectedGenderCheckIcon = Icons.check,
-      this.equallyAligned = true,
-      this.selectedGender,
-      this.checkIconAlignment = Alignment.bottomCenter,
-      @required this.onChanged,
-      this.linearGradient = balbirGradient,
-      this.maleImage = const AssetImage("assets/images/boy.png",
-          package: 'gender_selection'),
-      this.femaleImage = const AssetImage("assets/images/girl.png",
-          package: 'gender_selection'),
-      this.maleText = "Male",
-      this.femaleText = "Female"})
-      : super(key: key);
+  GenderSelection({
+    Key key,
+    this.selectedGenderTextStyle = const TextStyle(
+      fontSize: 19,
+      color: Colors.redAccent,
+      fontWeight: FontWeight.w600,
+    ),
+    this.unSelectedGenderTextStyle = const TextStyle(
+      fontSize: 19,
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+    ),
+    this.selectedGenderIconBackgroundColor = Colors.red,
+    this.selectedGenderIconColor = Colors.white,
+    this.selectedGenderIconSize = 20.0,
+    this.isSelectedGenderIconCircular = true,
+    this.animationDuration = const Duration(milliseconds: 500),
+    this.opacityOfGradient = 0.6,
+    this.padding = const EdgeInsets.all(10),
+    this.isCircular = true,
+    this.size = 120.0,
+    this.selectedGenderCheckIcon = Icons.check,
+    this.equallyAligned = true,
+    this.selectedGender,
+    this.checkIconAlignment = Alignment.bottomCenter,
+    @required this.onChanged,
+    this.linearGradient = balbirGradient,
+    this.maleImage = const AssetImage("assets/images/boy.png", package: 'gender_selection'),
+    this.femaleImage = const AssetImage("assets/images/girl.png", package: 'gender_selection'),
+    this.thirdGenderImage = const AssetImage("assets/images/third_gender.png", package: 'gender_selection'),
+    this.maleText = "Male",
+    this.femaleText = "Female",
+    this.thirdGenderText = "Third Gender",
+    this.enableThirdGender = false,
+  }) : super(key: key);
   @override
   _GenderSelectionState createState() => _GenderSelectionState();
 }
@@ -78,197 +82,117 @@ class _GenderSelectionState extends State<GenderSelection> {
           width: MediaQuery.of(context).size.width,
           child: Row(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: widget.equallyAligned
-                ? MainAxisAlignment.spaceEvenly
-                : MainAxisAlignment.center,
+            mainAxisAlignment: widget.equallyAligned ? MainAxisAlignment.spaceEvenly : MainAxisAlignment.center,
             children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    widget.selectedGender = Gender.Male;
-                    if (widget.onChanged != null) {
-                      widget.onChanged(widget.selectedGender);
-                    } else {
-                      return;
-                    }
-                  });
-                },
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Stack(
-                      fit: StackFit.loose,
-                      alignment: widget.checkIconAlignment,
-                      children: <Widget>[
-                        Container(
-                          height: widget.size,
-                          width: widget.size,
-                          margin: const EdgeInsets.only(top: 20, bottom: 10),
-                          decoration: BoxDecoration(
-                            shape: widget.isCircular
-                                ? BoxShape.circle
-                                : BoxShape.rectangle,
-                            image: DecorationImage(
-                              image: widget.maleImage,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          child: AnimatedOpacity(
-                            opacity: widget.selectedGender == Gender.Male
-                                ? widget.opacityOfGradient
-                                : 0.0,
-                            duration: widget.animationDuration,
-                            child: widget.selectedGender == Gender.Male
-                                ? Container(
-                                    decoration: BoxDecoration(
-                                        gradient: widget.linearGradient,
-                                        shape: widget.isCircular
-                                            ? BoxShape.circle
-                                            : BoxShape.rectangle),
-                                  )
-                                : null,
-                          ),
-                        ),
-                        Positioned(
-                          child: widget.selectedGenderCheckIcon != null
-                              ? AnimatedContainer(
-                                  duration: widget.animationDuration,
-                                  height: widget.selectedGender == Gender.Male
-                                      ? widget.selectedGenderIconSize
-                                      : 0.0,
-                                  width: widget.selectedGender == Gender.Male
-                                      ? widget.selectedGenderIconSize
-                                      : 0.0,
-                                  padding: const EdgeInsets.all(1),
-                                  curve: Curves.fastLinearToSlowEaseIn,
-                                  decoration: BoxDecoration(
-                                      shape: widget.isSelectedGenderIconCircular
-                                          ? BoxShape.circle
-                                          : BoxShape.rectangle,
-                                      color: widget
-                                          .selectedGenderIconBackgroundColor),
-                                  child: widget.selectedGender == Gender.Male
-                                      ? LayoutBuilder(
-                                          builder: (context, constraints) {
-                                            return Icon(
-                                              widget.selectedGenderCheckIcon,
-                                              color: widget
-                                                  .selectedGenderIconColor,
-                                              size: constraints.maxHeight,
-                                            );
-                                          },
-                                        )
-                                      : null)
-                              : Container(
-                                  height: 0,
-                                  width: 0,
-                                ),
-                        )
-                      ],
-                    ),
-                    Text(widget.maleText,
-                        style: widget.selectedGender == Gender.Male
-                            ? widget.selectedGenderTextStyle
-                            : widget.unSelectedGenderTextStyle)
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    widget.selectedGender = Gender.Female;
-                    if (widget.onChanged != null) {
-                      widget.onChanged(widget.selectedGender);
-                    } else {
-                      return;
-                    }
-                  });
-                },
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Stack(
-                      fit: StackFit.loose,
-                      alignment: widget.checkIconAlignment,
-                      children: <Widget>[
-                        Container(
-                          height: widget.size,
-                          width: widget.size,
-                          margin: const EdgeInsets.only(top: 20, bottom: 10),
-                          decoration: BoxDecoration(
-                              shape: widget.isCircular
-                                  ? BoxShape.circle
-                                  : BoxShape.rectangle,
-                              image: DecorationImage(
-                                  image: widget.femaleImage,
-                                  alignment: Alignment.center,
-                                  fit: BoxFit.cover)),
-                          child: AnimatedOpacity(
-                            opacity: widget.selectedGender == Gender.Female
-                                ? widget.opacityOfGradient
-                                : 0.0,
-                            duration: widget.animationDuration,
-                            child: widget.selectedGender == Gender.Female
-                                ? Container(
-                                    decoration: BoxDecoration(
-                                        gradient: widget.linearGradient,
-                                        shape: widget.isCircular
-                                            ? BoxShape.circle
-                                            : BoxShape.rectangle),
-                                  )
-                                : null,
-                          ),
-                        ),
-                        Positioned(
-                          child: widget.selectedGenderCheckIcon != null
-                              ? AnimatedContainer(
-                                  duration: widget.animationDuration,
-                                  height: widget.selectedGender == Gender.Female
-                                      ? widget.selectedGenderIconSize
-                                      : 0.0,
-                                  width: widget.selectedGender == Gender.Female
-                                      ? widget.selectedGenderIconSize
-                                      : 0.0,
-                                  padding: const EdgeInsets.all(1),
-                                  curve: Curves.fastLinearToSlowEaseIn,
-                                  decoration: BoxDecoration(
-                                      shape: widget.isSelectedGenderIconCircular
-                                          ? BoxShape.circle
-                                          : BoxShape.rectangle,
-                                      color: widget
-                                          .selectedGenderIconBackgroundColor),
-                                  child: widget.selectedGender == Gender.Female
-                                      ? LayoutBuilder(
-                                          builder: (context, constraints) {
-                                            return Icon(
-                                              widget.selectedGenderCheckIcon,
-                                              color: widget
-                                                  .selectedGenderIconColor,
-                                              size: constraints.maxHeight,
-                                            );
-                                          },
-                                        )
-                                      : null)
-                              : Container(
-                                  height: 0,
-                                  width: 0,
-                                ),
-                        )
-                      ],
-                    ),
-                    Text(widget.femaleText,
-                        style: widget.selectedGender == Gender.Female
-                            ? widget.selectedGenderTextStyle
-                            : widget.unSelectedGenderTextStyle)
-                  ],
-                ),
-              )
+              _genderBuilder(Gender.Male),
+              _genderBuilder(Gender.Female),
+              if (widget.enableThirdGender) _genderBuilder(Gender.ThirdGender),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Widget _genderBuilder(Gender _gender) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          widget.selectedGender = _gender;
+          if (widget.onChanged != null) {
+            widget.onChanged(widget.selectedGender);
+          } else {
+            return;
+          }
+        });
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Stack(
+            fit: StackFit.loose,
+            alignment: widget.checkIconAlignment,
+            children: <Widget>[
+              Container(
+                height: widget.size,
+                width: widget.size,
+                margin: const EdgeInsets.only(top: 20, bottom: 10),
+                decoration: BoxDecoration(
+                  shape: widget.isCircular ? BoxShape.circle : BoxShape.rectangle,
+                  image: DecorationImage(
+                    image: _getGenderImage(_gender),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: AnimatedOpacity(
+                  opacity: widget.selectedGender == _gender ? widget.opacityOfGradient : 0.0,
+                  duration: widget.animationDuration,
+                  child: widget.selectedGender == _gender
+                      ? Container(
+                          decoration: BoxDecoration(
+                              gradient: widget.linearGradient,
+                              shape: widget.isCircular ? BoxShape.circle : BoxShape.rectangle),
+                        )
+                      : null,
+                ),
+              ),
+              Positioned(
+                child: widget.selectedGenderCheckIcon != null
+                    ? AnimatedContainer(
+                        duration: widget.animationDuration,
+                        height: widget.selectedGender == _gender ? widget.selectedGenderIconSize : 0.0,
+                        width: widget.selectedGender == _gender ? widget.selectedGenderIconSize : 0.0,
+                        padding: const EdgeInsets.all(1),
+                        curve: Curves.fastLinearToSlowEaseIn,
+                        decoration: BoxDecoration(
+                            shape: widget.isSelectedGenderIconCircular ? BoxShape.circle : BoxShape.rectangle,
+                            color: widget.selectedGenderIconBackgroundColor),
+                        child: widget.selectedGender == _gender
+                            ? LayoutBuilder(
+                                builder: (context, constraints) {
+                                  return Icon(
+                                    widget.selectedGenderCheckIcon,
+                                    color: widget.selectedGenderIconColor,
+                                    size: constraints.maxHeight,
+                                  );
+                                },
+                              )
+                            : null)
+                    : Container(
+                        height: 0,
+                        width: 0,
+                      ),
+              )
+            ],
+          ),
+          Text(_getGenderText(_gender),
+              style:
+                  widget.selectedGender == _gender ? widget.selectedGenderTextStyle : widget.unSelectedGenderTextStyle)
+        ],
+      ),
+    );
+  }
+
+  _getGenderText(Gender gender) {
+    switch (gender) {
+      case Gender.Male:
+        return widget.maleText;
+      case Gender.Female:
+        return widget.femaleText;
+      case Gender.ThirdGender:
+        return widget.thirdGenderText;
+    }
+  }
+
+  _getGenderImage(Gender gender) {
+    switch (gender) {
+      case Gender.Male:
+        return widget.maleImage;
+      case Gender.Female:
+        return widget.femaleImage;
+      case Gender.ThirdGender:
+        return widget.thirdGenderImage;
+    }
   }
 }
